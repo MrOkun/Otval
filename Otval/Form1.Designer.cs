0a902488e493f24e234a43b178f7fef054d77555
MrOkun <74727162+MrOkun@users.noreply.github.com>
2021-09-21 22:34:37 +0300

    Добавьте файлы проекта.

---
+++ Otval/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Otval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.Color.White;
+            this.Picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Picture.Location = new System.Drawing.Point(0, 0);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(800, 450);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            this.Picture.Paint += new System.Windows.Forms.PaintEventHandler(this.Picture_Paint);
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Enabled = true;
+            this.UpdateTimer.Interval = 50;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.Picture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Timer UpdateTimer;
+    }
+}
+
---
+++ Otval/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otval
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _screenshot;
+        private Graphics _canvas;
+        private Random _rnd;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Start();
+        }
+
+        private void Start()
+        {
+            _rnd = new Random();
+
+            _canvas = Picture.CreateGraphics();
+
+            Bitmap myBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Picture.Image = myBitmap;
+            _canvas = Graphics.FromImage(Picture.Image);
+
+            this.WindowState = FormWindowState.Minimized;
+            _canvas.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+            _screenshot = new Bitmap(Picture.Image);
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.J)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            Picture.Invalidate();
+        }
+
+        private void Picture_Paint(object sender, PaintEventArgs e)
+        {
+            _canvas.Clear(Color.Black);
+
+            int SpanX, SpanY, SpanHeight;
+
+
+            if (_rnd.Next(0, 31) == 0)
+            {
+                _canvas.Clear(Color.Fuchsia);
+            }
+            else
+            {
+                _canvas.DrawImage(_screenshot, _rnd.Next(-5, 5), 0);
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                SpanHeight = _rnd.Next(4, 123);
+                if (_rnd.Next(0, 5) == 0)
+                {
+                    SpanX = _rnd.Next(-60, 60);
+                }
+                else
+                {
+                    SpanX = _rnd.Next(-15, 15);
+                }
+                SpanY = _rnd.Next(0, _screenshot.Height);
+
+                _canvas.DrawImage(_screenshot, new RectangleF(SpanX, SpanY, _screenshot.Width, SpanHeight), new RectangleF(0, SpanY, _screenshot.Width, SpanHeight), GraphicsUnit.Pixel);
+            }
+
+            for (int j = 0; j < _rnd.Next(0, 7); j++)
+            {
+                Pen Pen = new Pen(Color.White);
+
+                SpanY = _rnd.Next(_screenshot.Height);
+
+                if (_rnd.Next(0, 5) == 0)
+                {
+                    Pen.Color = Color.Fuchsia;
+                }
+                else
+                {
+                    Pen.Color = Color.Lime;
+                }
+
+                for (int i = 0; i < _rnd.Next(0, 40); i++)
+                {
+                    if (_rnd.Next(0, 3) == 0)
+                    {
+                        _canvas.DrawLine(Pen, 0, SpanY + i, _screenshot.Width, SpanY + i);
+                    }
+                }
+            }
+        }
+    }
+}
